f5d1d9a4256127deca956ec704d88be1130bf5a2
Ziemas <ziemas@ziemas.se>
2024-04-03 15:17:52 +0200

    util: need BIT to be ULL on ps2sdk

--- common/util.h
+++ common/util.h
@@ -1,7 +1,7 @@
 #ifndef UTIL_H_
 #define UTIL_H_
 
-#define BIT(x) (1UL << (x))
+#define BIT(x) (1ULL << (x))
 #define GENMASK(msb, lsb) ((BIT((msb + 1) - (lsb)) - 1) << (lsb))
 #define _FIELD_LSB(field) ((field) & ~(field - 1))
 #define FIELD_PREP(field, val) (((val) * (_FIELD_LSB(field))) & (field))
